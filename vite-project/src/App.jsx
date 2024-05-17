fbe4a91ff48ffed139acd96824862a6078463548
Brigitte Bolaños <brigittebedo@hotmail.com>
2024-05-16 19:18:36 -0500

    estructure project gallery

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>ESTAMOS PROBANDO</h1>
+       
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
